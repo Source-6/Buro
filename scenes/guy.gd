79a1dda63617298775bf4a2a350fcf95133fb428
source <vandermeirschmanon@gmail.com>
2026-01-16 13:28:40 +0100

    anim added, working on paper plane

--- scenes/guy.gd
+++ scenes/guy.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 @onready var guy: AnimatedSprite2D = $guy
 @onready var animation_player: AnimationPlayer = $guy/AnimationPlayer
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-@onready var timer: Timer = $Timer
+@onready var mug_timer: Timer = $MugTimer
 @onready var progress_bar: ProgressBar = $"../ProgressBar"
 @onready var mug_area_2d: Area2D = $"../MugArea2D"
 
@@ -48,24 +48,19 @@ func animate_collision_shape() -> void:
 
 func add_anger(anger_num : int) -> void:
 		progress_bar.value += anger_num
-		has_been_hit = true
-		guy.play("mug_hitting")
-		guy.flip_h = false
-		
-		#animation_player.play("hit")  make it softer
-		timer.start()
-		print(progress_bar.value)
+		if has_been_hit :
+			guy.play("mug_hitting")
+			mug_timer.start()
 
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if Input.is_action_just_pressed("throw"):
+	if Input.is_action_just_pressed("throw") && is_instance_valid(mug_area_2d):
 		mug_area_2d.throw_stuff()
 		if  mug_area_2d.throwed:
-			old_flip = guy.flip_h
+			has_been_hit = true
 			add_anger(10)
-			mug_area_2d.throwed = false
+			mug_area_2d.queue_free()
 
 
 func _on_timer_timeout() -> void:
 	guy.play("iddle")
-	guy.flip_h=old_flip
